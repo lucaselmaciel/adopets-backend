fdc711d4dbcc20c448fb6e71875f272e4de9e2d0
Edenilson Ferreira <edenilson.ferreira@gmail.com>
2021-04-21 22:11:15 -0300

    create pet and tutor models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Tutor(models.Model):
+    razao_social = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=14)
+    email = models.EmailField(max_length=50)
+    telefone = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.razao_social
+
+class Pet(models.Model):
+    
+    ESPECIES = [
+        ('0', "Cachorro"),
+        ('1', "Gato"),
+        ('2', "Pássaro"),
+    ]
+
+    PORTE = [
+        ('P', 'Pequeno'),
+        ('M', 'Médio'),
+        ('G', 'Grande'),
+    ]
+
+    SEXO = [
+        ('M', 'Masculino'),
+        ('F', 'Médio'),
+    ]
+
+    PESO = [
+        ('P','1 ~ 7kg'),
+        ('M','7 ~ 18kg'),
+        ('G','> 18kg'),
+    ]
+
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=100, blank=False, null=False)
+    especie = models.CharField(max_length=20, choices=ESPECIES)
+    raca = models.CharField(max_length=30)
+    idade = models.IntegerField()
+    sexo = models.CharField(max_length=10)
+    porte = models.CharField(max_length=20, choices=PORTE)
+    peso = models.CharField(max_length=20, choices=PESO)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.name
